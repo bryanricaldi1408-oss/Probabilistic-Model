a3393ec0a1f180f65541aac853b195c59c43cf24
Robert Saputra <robertsaputra2308@gmail.com>
2026-05-30 20:59:18 +0700

    Update skenario 2

--- src/Main.java
+++ src/Main.java
@@ -3,53 +3,66 @@ import indexing.InvertedIndex;
 import retrieval.BIMModel;
 import retrieval.SearchResult;
 import utils.FileLoader;
+import utils.QueryLoader;
+import utils.RelevanceLoader;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // path dataset
-        String datasetPath = "../dataset/Cranfield";
 
-        // load documents
+        // Load documents
         FileLoader loader = new FileLoader();
 
-        List<Document> documents = loader.loadDocuments(datasetPath);
+        List<Document> documents = loader.loadDocuments("../cran/cran.all.1400");
 
         System.out.println("Documents loaded: " + documents.size());
 
-        // build index
+        // Build inverted index
         InvertedIndex index = new InvertedIndex();
-
         index.build(documents);
 
-        System.out.println("Vocabulary size: " + index.getIndex().size());
+        System.out.println("Vocabulary size: " +index.getIndex().size());
 
-        System.out.println(
-                "Average document length: "
-                        + String.format(
-                                "%.2f",
-                                index.getAverageDocumentLength()));
+        System.out.println("Average document length: " + String.format("%.2f", index.getAverageDocumentLength()));
 
-        // initialize BIM
-        BIMModel bim = new BIMModel(index);
+        // Load queries
+        QueryLoader queryLoader = new QueryLoader();
+        queryLoader.loadQueries("../cran/cran.qry");
+
+        System.out.println("Queries loaded.");
+
+        // Load relevance judgement
+        RelevanceLoader relevanceLoader = new RelevanceLoader();
+
+        Map<Integer, Set<Integer>> relevanceMap = relevanceLoader.loadRelevance("../cran/cranqrel");
+
+        System.out.println("Relevance data loaded.");
+
+        // Initialize BIM
+        BIMModel bim = new BIMModel(index, relevanceMap);
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
 
             System.out.print("\nQuery: ");
-
             String query = scanner.nextLine();
-
             if (query.equalsIgnoreCase("exit")) {
                 break;
             }
 
-            List<SearchResult> results = bim.search(query);
+            // Auto match query
+            int matchedQueryId = queryLoader.findMostSimilarQuery(query);
+
+            System.out.println("Matched Cranfield Query ID: " + matchedQueryId);
+
+            List<SearchResult> results = bim.search(matchedQueryId, query);
 
             System.out.println("\n=== Top Results ===");
 
